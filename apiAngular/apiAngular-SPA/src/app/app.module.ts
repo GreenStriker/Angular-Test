4fb9254ef2ae57c91675cbbd0c6cf8e82e4c4c70
moukh rahman <moukhkst@hotmail.com>
2019-06-23 17:47:17 +0600

    angular interact with api

--- apiAngular/apiAngular-SPA/src/app/app.module.ts
+++ apiAngular/apiAngular-SPA/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ValueComponent } from './value/value.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
